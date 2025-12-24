2272e3edb3874db78ed4a412513e219c70ca3f68
andiartori <rizkyandiarto@gmail.com>
2025-12-24 20:14:24 +0700

    Add ACF JSON sync configuration

--- web/app/themes/applikasi-cbt/app/setup.php
+++ web/app/themes/applikasi-cbt/app/setup.php
@@ -153,3 +153,17 @@ add_action('widgets_init', function () {
         'id' => 'sidebar-footer',
     ] + $config);
 });
+
+/**
+ * Set custom ACF JSON save/load point
+ */
+
+add_filter('acf/settings/save_json', function ($path) {
+    return get_stylesheet_directory() . '/acf-json';
+});
+
+add_filter('acf/settings/load_json', function ($paths) {
+    unset($paths[0]);
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+    return $paths;
+});
